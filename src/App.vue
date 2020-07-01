5b529e6c3e17d4b670411cd20cdd098dcfb17715
taufiq <taufiq1689@gmail.com>
2020-07-01 08:35:32 +0800

    Home View done

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="white">
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo_only.png"
           transition="scale-transition"
           width="40"
         />
+        <v-toolbar-title class="mr-5">Cegah Covid</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn text>Data Indonesia</v-btn>
+        <v-btn text>Data Dunia</v-btn>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <div id="app">
+        <router-view />
+      </div>
     </v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
+}
 
-  components: {
-    HelloWorld,
-  },
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo_no_background.png" /> -->
+    <v-container>
+      <v-row class="justify-center align-center">
+        <v-col>
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink"
+            contain
+            src="../assets/logo.png"
+            transition="scale-transition"
+            height="500"
+            width="500"
+          />
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" color="white" max-width="400" max-height="400" elevation="0">
+            <v-card-title>
+              <span class="title font-weight-light">CegahCovid</span>
+            </v-card-title>
+
+            <v-card-text
+              class="headline text-left"  
+            >Aplikasi Untuk Melihat Data Penyebaran Covid-19 di Indonesia dan di Dunia.</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
-}
+};
 </script>
