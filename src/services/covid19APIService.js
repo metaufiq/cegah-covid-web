3e2895e01e177c865f1bb4054124c7ee16ebedcb
taufiq <taufiq1689@gmail.com>
2020-07-08 15:30:00 +0800

    fix bug covid19APIService

--- src/services/covid19APIService.js
+++ src/services/covid19APIService.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import url from "./DisableCorsURL";
 
-const apiEndPoint = url+'https://api.covid19api.com/';
+const apiEndPoint = url+'https://api.covid19api.com';
 
 const getWorldCoronaData = async () => {
     let response = await Axios.get(apiEndPoint + '/summary', {
